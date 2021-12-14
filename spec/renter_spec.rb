d57965151acc4c8deedf7c140e9cd10ac4cd9b6d
Katy Harrod <m.catherine.harrod@gmail.com>
2021-12-13 17:43:35 -0700

    Add Renter specs

---
+++ spec/renter_spec.rb
@@ -0,0 +1,18 @@
+require './lib/renter'
+
+RSpec.describe Renter do
+
+  # before :each do
+
+  it 'exists' do
+    renter = Renter.new("Patrick Star", "4242424242424242")
+    expect(renter).to be_a(Renter)
+  end
+
+  it 'has attributes' do
+    renter = Renter.new("Patrick Star", "4242424242424242")
+    expect(renter.name).to eq("Patrick Star")
+    expect(renter.credit_card_number).to eq("4242424242424242")
+  end 
+
+end
